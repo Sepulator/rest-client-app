2dc8169ba9f009dd11bfcc1c2db9d1c6dba5ce80
Anastasiia Nikonova <anastaney@gmail.com>
2025-09-03 14:06:38 +0200

    chore: add type-check script

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,17 +1,197 @@
-import { FlatCompat } from '@eslint/eslintrc';
+import js from '@eslint/js';
+import globals from 'globals';
+import reactRefresh from 'eslint-plugin-react-refresh';
+import tseslint, { configs as tsConfigs } from 'typescript-eslint';
+import stylistic from '@stylistic/eslint-plugin';
+import prettierConfig from 'eslint-config-prettier';
+import importPlugin from 'eslint-plugin-import-x';
+import perfectionistPlugin from 'eslint-plugin-perfectionist';
+import unicornPlugin from 'eslint-plugin-unicorn';
+import react from 'eslint-plugin-react';
+import reactDom from 'eslint-plugin-react-dom';
+import reactHooks from 'eslint-plugin-react-hooks';
+import * as reactCompiler from 'eslint-plugin-react-compiler';
+import jsxA11y from 'eslint-plugin-jsx-a11y';
+import next from '@next/eslint-plugin-next';
 
-const compat = new FlatCompat({
-  // import.meta.dirname is available after Node.js v20.11.0
-  baseDirectory: import.meta.dirname,
-});
-
-const eslintConfig = [
-  ...compat.config({
-    extends: ['next', 'prettier'],
-  }),
+export default tseslint.config(
+  tsConfigs.stylisticTypeChecked,
+  tsConfigs.strictTypeChecked,
+  unicornPlugin.configs.recommended,
   {
-    ignores: ['node_modules/**', '.next/**', 'out/**', 'build/**', 'next-env.d.ts'],
+    ignores: [
+      '**/*.js',
+      '**/*.config.js',
+      '**/*.config.ts',
+      '**/*.mjs',
+      '**/*.config.mjs',
+      'dist',
+      '.next',
+      'next-env.d.ts',
+    ],
   },
-];
-
-export default eslintConfig;
+  {
+    extends: [js.configs.recommended, ...tseslint.configs.recommended],
+    files: ['**/*.{ts,tsx}'],
+    languageOptions: {
+      ecmaVersion: 'latest',
+      globals: globals.browser,
+      sourceType: 'module',
+      parserOptions: {
+        projectService: true,
+        tsconfigRootDir: import.meta.dirname,
+      },
+    },
+    settings: {
+      'import-x/parsers': {
+        '@typescript-eslint/parser': ['.ts', '.tsx'],
+      },
+      'import-x/resolver': {
+        typescript: {
+          alwaysTryTypes: true,
+          project: './tsconfig.json',
+        },
+      },
+      react: {
+        version: 'detect',
+      },
+    },
+    plugins: {
+      react,
+      'react-hooks': reactHooks,
+      'react-dom': reactDom,
+      'react-refresh': reactRefresh,
+      'import-x': importPlugin,
+      perfectionist: perfectionistPlugin,
+      '@stylistic': stylistic,
+      'jsx-a11y': jsxA11y,
+      'react-compiler': reactCompiler,
+      '@next/next': next,
+    },
+    linterOptions: {
+      noInlineConfig: true,
+      reportUnusedDisableDirectives: 'error',
+    },
+    rules: {
+      ...react.configs.recommended.rules,
+      ...reactDom.configs.recommended.rules,
+      ...reactHooks.configs.recommended.rules,
+      ...next.configs.recommended.rules,
+      'react-compiler/react-compiler': 'error',
+      'react-refresh/only-export-components': ['warn', { allowConstantExport: true }],
+      ...jsxA11y.configs.recommended.rules,
+      'no-console': ['error', { allow: ['error'] }],
+      'no-empty': 'warn',
+      curly: ['error', 'all'],
+      'no-warning-comments': ['error', { terms: [''], location: 'anywhere' }],
+      'quote-props': ['error', 'always'],
+      'prefer-const': 'error',
+      'prefer-arrow-callback': 'error',
+      'no-confusing-arrow': ['error', { allowParens: true }],
+      '@typescript-eslint/prefer-readonly': 'error',
+      '@typescript-eslint/no-explicit-any': 'error',
+      '@typescript-eslint/no-magic-numbers': [
+        'error',
+        {
+          enforceConst: true,
+          ignoreEnums: true,
+          ignore: [0, 1, -1],
+          ignoreArrayIndexes: true,
+          ignoreDefaultValues: true,
+          ignoreClassFieldInitialValues: true,
+        },
+      ],
+      '@typescript-eslint/consistent-type-imports': 'error',
+      '@typescript-eslint/consistent-type-definitions': ['error', 'type'],
+      '@typescript-eslint/consistent-type-assertions': ['error', { assertionStyle: 'never' }],
+      '@typescript-eslint/explicit-member-accessibility': [
+        'error',
+        { accessibility: 'explicit', overrides: { constructors: 'off' } },
+      ],
+      '@typescript-eslint/member-ordering': 'error',
+      '@typescript-eslint/no-unused-vars': [
+        'error',
+        {
+          varsIgnorePattern: '^_',
+          argsIgnorePattern: '^_',
+        },
+      ],
+      ...prettierConfig.rules,
+      'import-x/extensions': [
+        'error',
+        {
+          ts: 'never',
+          tsx: 'never',
+          svg: 'always',
+          css: 'always',
+          webp: 'always',
+          gif: 'always',
+        },
+      ],
+      'import-x/no-extraneous-dependencies': ['error', { devDependencies: true }],
+      'import-x/no-cycle': ['error', { maxDepth: Infinity }],
+      'import-x/first': 'error',
+      'perfectionist/sort-imports': 'error',
+      '@stylistic/padding-line-between-statements': [
+        'error',
+        { blankLine: 'always', prev: '*', next: 'return' },
+        { blankLine: 'always', prev: ['const', 'let', 'var'], next: '*' },
+        {
+          blankLine: 'any',
+          prev: ['const', 'let', 'var'],
+          next: ['const', 'let', 'var'],
+        },
+        { blankLine: 'always', prev: ['case', 'default'], next: '*' },
+        { blankLine: 'always', prev: 'import', next: '*' },
+        { blankLine: 'any', prev: 'import', next: 'import' },
+      ],
+      'unicorn/no-array-callback-reference': 'off',
+      'unicorn/prefer-at': 'off',
+      'unicorn/no-array-for-each': 'off',
+      'unicorn/no-array-reduce': 'off',
+      'unicorn/no-null': 'off',
+      'unicorn/number-literal-case': 'off',
+      'unicorn/numeric-separators-style': 'off',
+      'unicorn/prefer-global-this': 'off',
+      'unicorn/prevent-abbreviations': [
+        'error',
+        {
+          allowList: {
+            acc: true,
+            env: true,
+            i: true,
+            j: true,
+            props: true,
+            Props: true,
+            args: true,
+            ImportMetaEnv: true,
+            ref: true,
+            Ref: true,
+            Params: true,
+          },
+        },
+      ],
+      'unicorn/filename-case': [
+        'error',
+        {
+          cases: {
+            kebabCase: true,
+          },
+          ignore: ['vite-env.d.ts'],
+        },
+      ],
+      'unicorn/prefer-string-raw': 'off',
+      'react/jsx-uses-react': 'off',
+      'react/react-in-jsx-scope': 'off',
+      'react/prop-types': 'off',
+      'jsx-a11y/click-events-have-key-events': 'off',
+      'jsx-a11y/no-noninteractive-element-interactions': 'off',
+    },
+  },
+  {
+    files: ['src/app/layout.tsx'],
+    rules: {
+      'react-refresh/only-export-components': 'off',
+    },
+  }
+);
