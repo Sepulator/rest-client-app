186459a8ebdb1fd8b269b95680352c0de8dd64b5
Yuri Skrypal <yjura@protonmail.com>
2025-09-03 13:04:54 +0300

    chore: vitest config

--- vitest.config.ts
+++ vitest.config.ts
@@ -4,7 +4,7 @@ export default defineConfig({
   test: {
     globals: true,
     environment: 'jsdom',
-    setupFiles: ['./src/__tests__/setup-tests.ts'],
+    // setupFiles: ['./src/__tests__/setup-tests.ts'],
     coverage: {
       reporter: 'text',
       thresholds: {
@@ -18,7 +18,7 @@ export default defineConfig({
         'src/**/*.test.{js,jsx,ts,tsx}',
         'src/**/*.spec.{js,jsx,ts,tsx}',
         'src/App.{js,jsx,ts,tsx}',
-        'src/__tests__/setup-tests.{js,ts,tsx}',
+        // 'src/__tests__/setup-tests.{js,ts,tsx}',
         'src/**/*.d.ts',
       ],
     },
