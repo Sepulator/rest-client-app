50e0b9d5f66fb208c585072d3ac63cf6b8e8dae1
Anastasiia Nikonova <anastaney@gmail.com>
2025-09-05 16:12:39 +0200

    feat: add form validation

--- src/app/[locale]/(auth)/login/page.tsx
+++ src/app/[locale]/(auth)/login/page.tsx
@@ -1,4 +1,4 @@
-import { LoginForm } from '@/components/login-form/login-form';
+import { LoginForm } from '@/components/login-form';
 
 const TEXTS = {
   INTRO: 'Welcome back! Please, login',
@@ -6,9 +6,9 @@ const TEXTS = {
 
 export default function LoginPage() {
   return (
-    <div>
+    <>
       <h1>{TEXTS.INTRO}</h1>
       <LoginForm />
-    </div>
+    </>
   );
 }
---
+++ src/components/login-form.tsx
@@ -0,0 +1,140 @@
+'use client';
+
+import { Form, Input, Button } from '@heroui/react';
+import { valibotResolver } from '@hookform/resolvers/valibot';
+import { useState, type FormEvent } from 'react';
+import { useForm } from 'react-hook-form';
+import * as v from 'valibot';
+
+import { MailIcon } from '@/components/icons/mail-icon';
+
+import { EyeFilledIcon } from './icons/eye-filled-icon';
+import { EyeSlashFilledIcon } from './icons/eye-slash-filled-icon';
+
+const TEXTS = {
+  EMAIL_LABEL: 'Email',
+  SUBMIT: 'Submit',
+  PASSWORD_LABEL: 'Password',
+  EMAIL_PLACEHOLDER: 'Enter your email',
+  PASSWORD_PLACEHOLDER: 'Enter your password',
+  PASSWORD_TOGGLE_VISIBILITY: 'toggle password visibility',
+};
+
+const VALIDATION_MESSAGES = {
+  MIN_LENGTH: 'Password must be at least 8 characters',
+  NUMBER: 'Password must contain at least one number',
+  LETTER: 'Password must contain at least one letter',
+  SPECIAL_CHARACTER: 'Password must contain at least one special character',
+  EMAIL_REQUIRED: 'Email is required',
+  EMAIL_INVALID: 'Email is invalid',
+};
+
+const MIN_PASSWORD_LENGTH = 8;
+
+const EMAIL_NAME = 'email';
+const PASSWORD_NAME = 'password';
+
+const passwordSchema = v.pipe(
+  v.string(),
+  v.regex(/\p{N}/u, VALIDATION_MESSAGES.NUMBER),
+  v.regex(/\p{L}/u, VALIDATION_MESSAGES.LETTER),
+  v.regex(/[^\p{L}\p{N}]/u, VALIDATION_MESSAGES.SPECIAL_CHARACTER),
+  v.minLength(MIN_PASSWORD_LENGTH, VALIDATION_MESSAGES.MIN_LENGTH)
+);
+
+const emailSchema = v.pipe(
+  v.string(),
+  v.nonEmpty(VALIDATION_MESSAGES.EMAIL_REQUIRED),
+  v.email(VALIDATION_MESSAGES.EMAIL_INVALID)
+);
+
+const loginSchema = v.object({
+  [EMAIL_NAME]: emailSchema,
+  [PASSWORD_NAME]: passwordSchema,
+});
+
+type LoginForm = v.InferInput<typeof loginSchema>;
+
+export const LoginForm = () => {
+  const [isPasswordVisible, setIsPasswordVisible] = useState(false);
+
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<LoginForm>({
+    mode: 'onChange',
+    reValidateMode: 'onChange',
+
+    criteriaMode: 'all',
+    resolver: valibotResolver(loginSchema),
+  });
+
+  const toggleVisibility = () => {
+    setIsPasswordVisible(!isPasswordVisible);
+  };
+
+  function onSubmit(event: FormEvent<HTMLFormElement>) {
+    const target = event.currentTarget;
+
+    void handleSubmit((data) => {
+      event.preventDefault();
+
+      console.log('data:', data);
+
+      target.submit();
+    })(event);
+  }
+
+  return (
+    <Form
+      className="flex w-full max-w-xs flex-col gap-4"
+      onSubmit={(event) => {
+        onSubmit(event);
+      }}
+    >
+      <Input
+        endContent={<MailIcon className="text-default-400 pointer-events-none shrink-0 text-2xl" />}
+        className="w-full"
+        autoComplete="email"
+        label={TEXTS.EMAIL_LABEL}
+        labelPlacement="outside"
+        placeholder="you@example.com"
+        {...register(EMAIL_NAME)}
+        errorMessage={errors.email?.message}
+        isInvalid={!!errors.email}
+        type="email"
+      />
+
+      <Input
+        autoComplete="current-password"
+        {...register(PASSWORD_NAME)}
+        endContent={
+          <button
+            aria-label={TEXTS.PASSWORD_TOGGLE_VISIBILITY}
+            className="cursor-pointer outline-transparent focus:outline-solid"
+            type="button"
+            onClick={toggleVisibility}
+          >
+            {isPasswordVisible ? (
+              <EyeSlashFilledIcon className="text-default-400 pointer-events-none text-2xl" />
+            ) : (
+              <EyeFilledIcon className="text-default-400 pointer-events-none text-2xl" />
+            )}
+          </button>
+        }
+        labelPlacement="outside"
+        label={TEXTS.PASSWORD_LABEL}
+        placeholder={TEXTS.PASSWORD_PLACEHOLDER}
+        errorMessage={errors.password?.message}
+        isInvalid={!!errors.password}
+        type={isPasswordVisible ? 'text' : 'password'}
+      />
+      <div className="flex gap-2">
+        <Button color="primary" type="submit">
+          {TEXTS.SUBMIT}
+        </Button>
+      </div>
+    </Form>
+  );
+};
