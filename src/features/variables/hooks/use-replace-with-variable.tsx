cec2035e1b1809fc9a9bf0d6b9c850ecf6cfecbe
Maria Ilina <m.polibit@gmail.com>
2025-09-18 13:48:19 +0300

    feat: add sync for cross tabs

--- src/features/variables/hooks/use-replace-with-variable.tsx
+++ src/features/variables/hooks/use-replace-with-variable.tsx
@@ -1,21 +1,35 @@
-import { useCallback, useMemo } from 'react';
+'use client';
+
+import { useCallback, useEffect, useState } from 'react';
 
 import { useVariables } from '@/stores/variables/selectors';
+import { STORAGE_KEY } from '@/stores/variables/store';
 
 export function useReplaceWithVariable() {
   const variables = useVariables();
+  const [_, setStorageVersion] = useState(0);
 
-  const variablesObject = useMemo(
-    () =>
-      variables.reduce<Record<string, string>>((acc, { key, value }) => {
-        if (key) {
-          acc[key] = value;
-        }
+  const variablesObject = variables.reduce<Record<string, string>>((acc, { key, value }) => {
+    if (key) {
+      acc[key] = value;
+    }
 
-        return acc;
-      }, {}),
-    [variables]
-  );
+    return acc;
+  }, {});
+
+  useEffect(() => {
+    const syncWithStorage = (event: StorageEvent) => {
+      if (event.key === STORAGE_KEY && event.newValue) {
+        setStorageVersion((previous) => previous + 1);
+      }
+    };
+
+    window.addEventListener('storage', syncWithStorage);
+
+    return () => {
+      window.removeEventListener('storage', syncWithStorage);
+    };
+  }, []);
 
   const replaceWithValue = useCallback(
     (string: string) => {
