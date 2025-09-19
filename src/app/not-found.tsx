da21ae8a8c02927e44a71606a6ff21a6b5851622
Maria Ilina <m.polibit@gmail.com>
2025-09-19 12:19:34 +0300

    feat: add root not-found

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,14 +1,25 @@
-'use client';
+import { cn } from '@heroui/react';
+import Link from 'next/link';
 
-import Error from 'next/error';
+import './[locale]/globals.css';
+import { geistMono, geistSans } from '@/app/[locale]/fonts';
+import { ROUTES } from '@/config/routes';
 
-const NOT_FOUND_CODE = 404;
-
-export default function NotFound() {
+export default function GlobalNotFound() {
   return (
     <html lang="en">
-      <body>
-        <Error statusCode={NOT_FOUND_CODE} />
+      <body className={cn(geistSans.variable, geistMono.variable, 'dark text-foreground bg-background antialiased')}>
+        <section className="flex h-screen w-full max-w-screen-2xl flex-1 flex-col items-center justify-center">
+          <span className="text-4xl">404</span>
+          <h1>Global Not Found</h1>
+          <p>Non-localized</p>
+          <Link
+            className="bg-primary-300 hover:bg-primary/90 rounded-medium mt-8 w-50 px-5 py-2 text-center text-sm font-medium transition-colors"
+            href={ROUTES.MAIN}
+          >
+            Go Home
+          </Link>
+        </section>
       </body>
     </html>
   );
